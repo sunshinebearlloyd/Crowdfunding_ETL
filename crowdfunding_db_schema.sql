d026503656f3df3ec0589bd40ce0875bf3b35ff6
sunshinebearlloyd <sunshinebearlloyd@gmail.com>
2023-09-11 16:26:00 -0500

    changed int32 to int64 in option 2 updated outputs and updated sql file

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -29,9 +29,9 @@ CREATE TABLE "campaign" (
 
 CREATE TABLE "contacts" (
     "contact_id" INT   NOT NULL,
-    "email" VARCHAR(255)   NOT NULL,
     "first_name" VARCHAR(255)   NOT NULL,
-    "last_name" VARCHAR(255)   NOT NULL,
+    "last_name" VARCHAR(255)   NOT NULL,    
+    "email" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_contacts" PRIMARY KEY (
         "contact_id"
      )
